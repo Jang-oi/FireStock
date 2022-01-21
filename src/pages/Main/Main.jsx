2c22d5ad6ac508cc73fbd214f77b47434491f6aa
Jang-oi <jjh53241@gmail.com>
2022-01-21 23:54:33 +0900

    메인페이지

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,8 +1,9 @@
-import {Card, CardContent, Container, Typography} from '@mui/material';
+import {Avatar, Card, CardContent, Container, Grid, Typography} from '@mui/material';
 import {useEffect, useState} from "react";
 import {axiosCall} from "../../utils/commonUtil";
 import {useSelector} from "react-redux";
 import {getStockArray} from "../../utils/arrayUtil";
+import MoneyIcon from '@mui/icons-material/Money';
 
 const Main = () => {
 
@@ -15,8 +16,8 @@ const Main = () => {
     useEffect(() => {
         axiosCall.get('/portfolio/find/folio', {userId: userInfo._id}, function (returnData) {
             const testArray = Object.values(returnData.portFolioDetailMap);
-            const test = [];
-            for(let i = 0; i < testArray.length; i++) {
+            let test = [];
+            for (let i = 0; i < testArray.length; i++) {
                 test.push(getStockArray(testArray[i].portFolioDataList, coinData))
             }
             console.log(test);
@@ -25,18 +26,56 @@ const Main = () => {
         })
     }, [userInfo._id])
 
+    const assetArray = ['국내 주식', '해외 주식', '암호 화폐', '비유동 자산'];
+
     return (
         <Container>
-            <Card sx={{height: '100%', mb: 3}}>
-                <CardContent>
-                        <Typography color="textSecondary" gutterBottom variant="overline">
-                            총 자산
-                        </Typography>
-                        <Typography color="textPrimary" variant="h4">
-                            $24k
-                        </Typography>
-                </CardContent>
-            </Card>
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <Card sx={{height: '100%', mb: 3}}>
+                        <CardContent>
+                            <Typography color="textSecondary" gutterBottom variant="overline">
+                                총 자산
+                            </Typography>
+                            <Typography color="textSecondary" gutterBottom variant="overline" sx={{float:'right'}}>
+                                환율 {window.basePrice}
+                            </Typography>
+                            <Typography color="textPrimary" variant="h4">
+                                $24k
+                            </Typography>
+
+                        </CardContent>
+                    </Card>
+                </Grid>
+                {assetArray.map((value, index) => {
+                    return (
+                        <Grid item xs={6} key={index}>
+                            <Card sx={{height: '100%', mb: 3}}>
+                                <CardContent>
+                                    <Typography color="textSecondary" gutterBottom variant="overline">
+                                        {value}
+                                    </Typography>
+                                    <Typography color="textPrimary" variant="h4">
+                                        $24k
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    )
+                })}
+                <Grid item xs={12}>
+                    <Card sx={{height: '100%', mb: 3}}>
+                        <CardContent>
+                            <Typography color="textSecondary" gutterBottom variant="overline">
+                                현금성 자산
+                            </Typography>
+                            <Typography color="textPrimary" variant="h4">
+                                $24k
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
         </Container>
     )
 }
